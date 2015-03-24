b47b004c5709b408010139a2fbe927d31379650f
Kestutis Baltronas <kestutisbalt@gmail.com>
2015-03-24 21:06:30 +0200

    Added command line argument parsing.

--- src/llvm_list_functions_sample.cpp
+++ src/llvm_list_functions_sample.cpp
@@ -1,5 +1,14 @@
+#include <string>
+
+#include <llvm/Support/CommandLine.h>
+
+
 int
 main(int argc, char *args[])
 {
-	return 0;
+	llvm::cl::opt<std::string> file_name(llvm::cl::Positional,
+		llvm::cl::desc("bitcode_file"), llvm::cl::Required);
+
+	llvm::cl::ParseCommandLineOptions(argc, args,
+		"Lists functions from LLVM bitcode\n");
 }
