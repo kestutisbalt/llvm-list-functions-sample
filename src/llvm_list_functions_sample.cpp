7fc26151bb9fcd066f7892f031d63cd6339c9324
Kestutis Baltronas <kestutisbalt@gmail.com>
2015-03-25 22:11:51 +0200

    Reverted changes due to the bad memory management.

--- src/llvm_list_functions_sample.cpp
+++ src/llvm_list_functions_sample.cpp
@@ -1,23 +1,9 @@
 #include <string>
+#include <vector>
 #include <iostream>
 
 #include <llvm/Support/CommandLine.h>
 #include <llvm/Support/MemoryBuffer.h>
-
-
-std::unique_ptr<llvm::MemoryBuffer>&
-get_memory_buffer_for_file(std::string file_name)
-{
-	llvm::ErrorOr<std::unique_ptr<llvm::MemoryBuffer>> memory_buffer =
-		llvm::MemoryBuffer::getFile(file_name);
-
-	if (memory_buffer.getError())
-	{
-		throw std::runtime_error("Cannot open bitcode file");
-	}
-
-	return memory_buffer.get();
-}
 
 
 int
@@ -28,13 +14,4 @@ main(int argc, char *args[])
 
 	llvm::cl::ParseCommandLineOptions(argc, args,
 		"Lists functions from LLVM bitcode\n");
-
-	try
-	{
-		get_memory_buffer_for_file(file_name).get();
-	}
-	catch(const std::runtime_error& error)
-	{
-		std::cerr <<error.what() <<"\n";
-	}
 }
